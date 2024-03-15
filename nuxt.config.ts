0fdc13f8df511ca38b80dbf989de27c4da269eb9
kelegele <felicis1024@gmail.com>
2024-03-15 11:17:34 +0800

    add modules i18n,seo,sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'baidu-site-verification',
+          content: 'codeva-vxn7O0s0kt'
+        }
+      ]
+    }
+  },
+  site: {
+    url: 'https://kelemc.fun',
+    name: 'KeleMC 可勒世界',
+    description: 'Welcome to KeleMC!',
+    defaultLocale: 'cn' // not needed if you have @nuxtjs/i18n installed
+  },
   devtools: { enabled: true },
-  css:['~/assets/css/normalize.css']
+  css: ['~/assets/css/normalize.css'],
+  modules: ['@nuxtjs/i18n', '@nuxtjs/seo', '@nuxtjs/sitemap']
 })
